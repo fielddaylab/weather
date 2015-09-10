62bd14a2e91ec9a10a30d3f53ab05e4a3513b650
Phildo <Phildo211@gmail.com>
2015-09-10 09:53:52 -0500

    heightmap

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -2,7 +2,41 @@ var GamePlayScene = function(game, stage)
 {
   var self = this;
 
-  var Field2d = function(w,h)
+  var HeightMap = function(w,h)
+  {
+    var self = this;
+    self.w = w;
+    self.h = h;
+    self.buffs = [];
+    self.buffs[0] = [];
+    self.buffs[1] = [];
+    for(var i = 0; i < w*h; i++) self.buffs[0][i] = Math.random();
+    for(var i = 0; i < w*h; i++) self.buffs[1][i] = Math.random();
+    self.buff = 0;
+    self.data = self.buffs[self.buff];
+
+    self.iFor = function(x,y) { return (y*w)+x; }
+    self.anneal = function()
+    {
+      var oldb = self.buff;
+      var newb = (self.buff+1)%2;
+      for(var y = 0; y < h; y++)
+      {
+        for(var x = 0; x < w; x++)
+        {
+          var index = self.iFor(x,y);
+          self.buffs[newb][index] = self.buffs[oldb][index];
+          self.buffs[newb][index] += self.buffs[oldb][self.iFor(((x-1)+w)%w,y)];
+          self.buffs[newb][index] += self.buffs[oldb][self.iFor(((x+1)+w)%w,y)];
+          self.buffs[newb][index] += self.buffs[oldb][self.iFor(x,((y-1)+h)%h)];
+          self.buffs[newb][index] += self.buffs[oldb][self.iFor(x,((y+1)+h)%h)];
+          self.buffs[newb][index] /= 5;
+        }
+      }
+      self.buff = (self.buff+1)%2;
+    }
+  }
+  var VecField2d = function(w,h)
   {
     var self = this;
     self.w = w;
@@ -19,11 +53,18 @@ var GamePlayScene = function(game, stage)
     self.iFor = function(x,y) { return (y*(w*2))+(x*2); }
     self.applyForce = function(xs,ys,xe,ye)
     {
+      for(var i = 0; i < h; i++)
+      {
+        for(var j = 0; j < w; j++)
+        {
 
+        }
+      }
     }
   }
 
-  self.field;
+  self.hmap;
+  self.vfield;
 
 
 
@@ -37,7 +78,8 @@ var GamePlayScene = function(game, stage)
 
   self.ready = function()
   {
-    self.field = new Field2d(50,50);
+    self.hmap   = new HeightMap(50,50);
+    self.vfield = new VecField2d(50,50);
   };
 
   self.tick = function()
@@ -47,24 +89,42 @@ var GamePlayScene = function(game, stage)
   self.draw = function()
   {
     var canv = stage.drawCanv;
-    var x_space = canv.canvas.width / self.field.w;
-    var y_space = canv.canvas.height / self.field.h;
+    canv.context.lineWidth = 0.5;
+
+    var x_space;
+    var y_space;
     var x;
     var y;
     var index;
-    canv.context.lineWidth = 0.5;
-    for(var i = 0; i < self.field.h; i++)
+
+    x_space = canv.canvas.width / self.hmap.w;
+    y_space = canv.canvas.height / self.hmap.h;
+    for(var i = 0; i < self.hmap.h; i++)
     {
-      for(var j = 0; j < self.field.w; j++)
+      for(var j = 0; j < self.hmap.w; j++)
       {
         y = y_space*i;
         x = x_space*j;
-        index = self.field.iFor(j,i);
+        index = self.hmap.iFor(j,i);
+        var color = Math.round(self.hmap.data[index]*255);
+        canv.context.fillStyle = "rgba("+color+","+color+","+color+",1)";
+        canv.context.fillRect(x,y,x_space,y_space);
+      }
+    }
+
+    x_space = canv.canvas.width / self.vfield.w;
+    y_space = canv.canvas.height / self.vfield.h;
+    for(var i = 0; i < self.vfield.h; i++)
+    {
+      for(var j = 0; j < self.vfield.w; j++)
+      {
+        y = y_space*i;
+        x = x_space*j;
+        index = self.vfield.iFor(j,i);
         canv.context.strokeStyle = "#ff0000";
-        //if(i == 10 && j == 10)
         canv.context.strokeRect(x-0.5,y-0.5,1,1);
         canv.context.strokeStyle = "#000000";
-        canv.drawLine(x,y,x+self.field.data[index],y+self.field.data[index+1]);
+        canv.drawLine(x,y,x+self.vfield.data[index],y+self.vfield.data[index+1]);
       }
     }
   };
