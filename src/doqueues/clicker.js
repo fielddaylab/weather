99ac79b8d7dae5eaf8a904c9e5d7c3629ae02b55
Phildo <Phildo211@gmail.com>
2015-12-01 15:46:06 -0600

    removed touch debugs git st

--- src/doqueues/clicker.js
+++ src/doqueues/clicker.js
@@ -29,7 +29,6 @@ var Clicker = function(init)
   function click(evt)
   {
     doSetPosOnEvent(evt);
-    d.log(evt.doX+","+evt.doY);
     for(var i = 0; i < clickables.length; i++)
     {
       if(clicked(clickables[i], evt))
