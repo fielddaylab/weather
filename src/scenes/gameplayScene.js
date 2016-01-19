c6eb0550920044b29ce458460889344ffd690a9d
Phildo <Phildo211@gmail.com>
2016-01-19 14:16:38 -0600

    complete message

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -1757,15 +1757,15 @@ var GamePlayScene = function(game, stage)
     {
       canv.context.font = "30px stump";
       canv.context.textAlign = "center";
-      canv.outlineText("Complete!",canv.canvas.width/2,100,"#000000","#FFFFFF");
+      canv.outlineText("Complete!",canv.canvas.width/2,100+Math.sin(self.ticks/10)*10,"#000000","#FFFFFF");
     }
     else if(self.levels[self.cur_level].timer > 0)
     {
       canv.context.font = "30px stump";
       canv.context.textAlign = "right";
-      canv.outlineText("Time to Complete: ",canv.canvas.width/2,100,"#000000","#FFFFFF");
+      canv.outlineText("Time to Complete: ",canv.canvas.width/2+100,100+Math.sin(self.ticks/10)*10,"#000000","#FFFFFF");
       canv.context.textAlign = "left";
-      canv.outlineText(" "+(Math.round(30-((self.levels[self.cur_level].timer/self.levels[self.cur_level].req_timer)*30))/10),canv.canvas.width/2,100,"#000000","#FFFFFF");
+      canv.outlineText(" "+(Math.round(30-((self.levels[self.cur_level].timer/self.levels[self.cur_level].req_timer)*30))/10),canv.canvas.width/2+100,100+Math.sin(self.ticks/10)*10,"#000000","#FFFFFF");
     }
 
     canv.context.drawImage(screen_cover_img,0,0,canv.canvas.width,canv.canvas.height);
